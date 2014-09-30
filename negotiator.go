3ee0058ba0f2c0f4a7de39ff77e1322add740d63
Kyle Boorky <kboorky@turbine.com>
2014-09-30 14:11:51 -0400

    can now dynamically add encoders

--- negotiator.go
+++ negotiator.go
@@ -10,6 +10,7 @@ const MimeXML = "application/xml"
 
 // Encoder is an interface for a struct that can encode data into []byte
 type Encoder interface {
+	PrettyPrint bool
 	Encode(data interface{}) ([]byte, error)
 	ContentType() string
 }
@@ -21,31 +22,49 @@ type Negotiator interface {
 }
 
 // ContentNegotiator is a Neotiator that supports pretty printing
-// and a fallback/default encoder
+// and a fallback/default encoder as well as dynamically adding
+// encoders
 type ContentNegotiator struct {
 	PrettyPrint    bool
 	DefaultEncoder Encoder
 	ResponseWriter http.ResponseWriter
+	encoderMap     map[string]Encoder
+}
+
+func NewJsonXmlContentNegotiator(prettyPrint bool, defaultEncoder Encoder, responseWriter http.ResponseWriter) ContentNegotiator {
+	result := ContentNegotiator{prettyPrint, defaultEncoder, responseWriter}
+	result.AddEncoder(MimeJSON, JsonEncoder{prettyPrint})
+	result.AddEncoder(MimeXML, XmlEncoder{prettyPrint})
+	return result
 }
 
 // Negotiate inspects the request for the accept header and
 // encodes the response appropriately.
 func (cn ContentNegotiator) Negotiate(req *http.Request, data interface{}) ([]byte, error) {
+	if len(cn.encoderMap) <= 0 {
+		panic("No Encoders present. Please add them using ContentNegotiator.AddEncoder()")
+	}
 	var e = cn.getEncoder(req)
 	cn.ResponseWriter.Header().Set("Content-Type", e.ContentType())
 	return e.Encode(data)
+}
+
+// AddEncoder registers a mimetype and its encoder to be used if a client
+// requests that mimetype
+func (cn ContentNegotiator) AddEncoder(mimeType string, enc Encoder) {
+	cn.encoderMap[mimeType] = enc
 }
 
 // getEncoder parses the Accept header an returns the appropriate encoder to use
 func (cn ContentNegotiator) getEncoder(req *http.Request) Encoder {
 	var result = cn.DefaultEncoder
 	accept := req.Header.Get("Accept")
-	if strings.Contains(accept, MimeXML) {
-		//get the xml encoder
-		result = XmlEncoder{cn.PrettyPrint}
-	} else if strings.Contains(accept, MimeJSON) {
-		//default to json
-		result = JsonEncoder{cn.PrettyPrint}
+
+	for k, v := range cn.encoderMap {
+		if strings.Contains(accept, k) {
+			result = v
+			break
+		}
 	}
 	return result
 }
